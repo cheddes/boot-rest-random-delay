a76cc7b598ed6a3a3f2112a9b45eacf31b016e29
dev1 <dev1@hcl.com>
2016-08-17 01:20:31 -0700

    initial content of the project.

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+HelloWorld RESTful service with SpringBoot framework.
+===================
+Follow below instructions to build and run:
+	* To build, run: mvn clean package
+	* To run using maven, use: mvn spring-boot:run
+	* To run using java, use: java -Dserver.port=8088 -jar target/gs-rest-service-0.1.0.jar
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) throws Exception {
+    	Random random = new Random();
+		Thread.sleep(random.nextInt(1000));
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
